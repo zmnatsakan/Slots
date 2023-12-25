bd936af3446156e6196080431eccd3ad2d502fa0
zmnatsakan <pmmnacakan11crew@gmail.com>
2023-12-25 15:04:29 +0300

    Initial Commit

---
+++ slots/slotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  slotsApp.swift
+//  slots
+//
+//  Created by mnats on 25.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct slotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
